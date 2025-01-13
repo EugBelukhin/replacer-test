22b5bee736e77b6319afac0dbbd1df607b115900
EugBelukhin <belhellimpala@gmail.com>
2025-01-13 15:08:31 +0300

    Create testscript.user.js

---
+++ testscript.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Test Script for Tampermonkey
+// @namespace    http://example.com/
+// @version      1.0.0
+// @description  This is a test script for Tampermonkey auto-update.
+// @author       YourName
+// @match        https://example.com/*
+// @updateURL    https://raw.githubusercontent.com/yourusername/yourrepo/main/testscript.user.js
+// @downloadURL  https://raw.githubusercontent.com/yourusername/yourrepo/main/testscript.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('Test script version 1.0.0 loaded.');
+})();
